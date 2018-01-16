9aa9ea4fa243067d9d408f5258fb48259db9983a
Jude Grodesky <jgrodesky@stuy.edu>
2018-01-16 05:19:51 -0500

    Winnings added to Game

--- v3/Game.java
+++ v3/Game.java
@@ -5,13 +5,19 @@ public abstract class Game {
     protected double cost;
 
     protected double mult;
-    
+
+    protected double winnings;
+
     public double getCost(){
 	return cost;
     }
 
     public double getMult(){
 	return mult;
+    }
+
+    public double getWinnings(){
+	return winnings;
     }
     
     public abstract String about();
