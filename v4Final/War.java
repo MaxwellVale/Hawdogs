38110d83fffc603d7e3d80939bdd4502699bf6ce
Mackswehl <max.s.vale01@gmail.com>
2018-01-17 01:56:06 -0500

    Took care of case in which Player does not pick yes/no for rules

--- v4Final/War.java
+++ v4Final/War.java
@@ -27,6 +27,13 @@ public class War extends Game {
     if (rules == 1) {
       System.out.println(about());
     }
+    else if (rules == 2) {
+      System.out.println("\nAlright we get it, you're a pro...\n");
+    }
+    else {
+      System.out.println("\nSomething tells me you're confused, so here are the rules anyway...\n");
+      System.out.println(about());
+    }
 
     play();
   }
